4875d72dc79549a53d62707c8c612cf63ecd5a2d
atoshUser <abdurahmonavezdjanov031@gmail.com>
2023-05-23 14:09:37 +0500

    add all fnc and last user list category

--- js/app.js
+++ js/app.js
@@ -1,20 +1,43 @@
+//const { log } = require("console");
+
 const form = document.getElementById("form");
 const formButton = document.getElementById("form__button");
 const user = document.getElementById("user");
 const deleteBtn = document.getElementById("delete__btn");
 const clearBtn = document.getElementById("clear__button");
 
+form["form__input"].addEventListener("input", () => {
+    const inputValue = form["form__input"].value.toLowerCase();
+    const userNames = document.querySelectorAll(".user__name");
+    userNames.forEach((userName) => {
+        if (
+            userName.lastElementChild.textContent
+                .toLowerCase()
+                .includes(inputValue)
+        ) {
+            userName.parentElement.style.display = "block";
+        } else {
+            userName.parentElement.style.display = "none";
+        }
+    });
+});
+
 formButton.addEventListener("click", (e) => {
     e.preventDefault();
     reload();
+    clearBtn.style.visibility = "visible";
 });
 
+clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    user.innerHTML = "";
+    clearBtn.style.visibility = "hidden";
+});
 /* get data and update UI */
 
 const updateUI = (info) => {
     user.innerHTML = "";
     info.forEach((element) => {
-        console.log(element);
         const { gender, picture, name, location, dob } = element;
         user.innerHTML += `
         <li class="user__item">
@@ -58,3 +81,13 @@ const updateUI = (info) => {
         `;
     });
 };
+
+user.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.id == "delete__btn") {
+        target.parentElement.remove();
+    }
+    if (!user.childElementCount) {
+        clearBtn.style.visibility = "hidden";
+    }
+});
